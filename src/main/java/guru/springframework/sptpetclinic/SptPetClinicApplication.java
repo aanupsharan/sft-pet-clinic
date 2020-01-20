be05fd774d144e4b79b4fd04eb24100589836105
aanupsharan <anup.ambula@gmail.com>
2020-01-20 11:58:22 -0600

    initial spring web project

---
+++ src/main/java/guru/springframework/sptpetclinic/SptPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sptpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sptpetclinic/SptPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sptpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
